f606c0885fbc57d27d35a88fc2dcbd793d15eb29
Jerome Colin <jerome.colin64100@gmail.com>
2019-07-11 10:22:49 +0200

    fonction avec return

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+include '/Users/jeromecolin/work/Biarritz_Atelier3_0719/SRC/function.php';
+function sayHello($i)
+{
+    
+    return $i;
+}
+echo sayHello("Hello Nite Owl");
+
+
+
+
+
+
+
+
+?>
