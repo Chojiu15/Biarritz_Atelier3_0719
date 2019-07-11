04b24c35eb7f6544b2c8b95664545911080a7a96
zepolclem <lopez.clmnt@gmail.com>
2019-07-11 10:22:53 +0200

    Exercice termine

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
 include "../src/function.php";
+
+
+$watchmen = ["Dr . Manhattan ", "Ozymandias", "Silk Spectre",  "Rorschach",  "The comedian",  "Nite Owl"];
+
+
+// var_dump($watchmen);
+
+
+
+
+echo sayHello("Toto");
+echo "<br>";
+echo whoAmI("Rorschach", $watchmen);
